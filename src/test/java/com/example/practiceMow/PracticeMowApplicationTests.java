6f81f7a33a589d22e86308c08af52cc5589dfb04
leemhmh <91586116+leemhmh@users.noreply.github.com>
2024-05-23 23:50:43 +0900

    spring project upload

---
+++ src/test/java/com/example/practiceMow/PracticeMowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practiceMow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
